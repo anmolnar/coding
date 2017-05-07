60b2340a278ba27a8669a6e1eab2920092a90037
Andor Molnár <andor.molnar@logmein.com>
2017-05-07 09:00:37 +0200

    Ransom finished

--- RansomNote.java
+++ RansomNote.java
@@ -3,16 +3,19 @@ import java.util.*;
 
 public class RansomNote {
 	public static void main(String[] args) throws IOException {
-		if (args.length < 1) {
-			System.out.println("Usage: ransomNote <text file>");
+		if (args.length < 2) {
+			System.out.println("Usage: ransomNote <text file> <ransom note>");
 			return;
 		}
-	
-		System.out.println("Loading: " + args[0]);
+
+		String magazine = args[0];
+		String ransomNote = args[1];
+
+		System.out.println("Ransom note: " + ransomNote);	
+		System.out.println("Loading: " + magazine);
 
 		int[] karakan = new int[256];
-
-		try (BufferedReader reader = new BufferedReader(new FileReader(args[0]))) {
+		try (BufferedReader reader = new BufferedReader(new FileReader(magazine))) {
 			int ch = reader.read();
 			while (ch != -1) {
 				karakan[ch]++;
@@ -20,10 +23,20 @@ public class RansomNote {
 			}
 		}	
 		catch (FileNotFoundException e) {
-			System.out.println("File not found: " + args[0]);
+			System.out.println("File not found: " + e.getMessage());
 			return;
 		}
 
-		System.out.println(Arrays.toString(karakan));
+		for (int i = 0; i < ransomNote.length(); i++) {
+			int kar = ransomNote.charAt(i);
+			if (karakan[kar] == 0) {
+				System.out.printf("Ransom note cannot be constructed. Not enough '%c' char.", kar);
+				return;
+			}
+			karakan[kar]--;
+		}
+
+		System.out.println("OK");
 	}
 }
+
